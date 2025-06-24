b903f593dbf9ff1afc6b38ac87ebc3dddebd538a
muriloapp <muriloandrep@gmail.com>
2025-06-24 18:20:19 -0400

    exploratory

--- src/exploratory.R
+++ src/exploratory.R
@@ -5,53 +5,105 @@ source(here::here("src","config.R"))
 
 ######################################################################
 
-
 res_stand <- readRDS("simul_results/static_dgp/standard_stat_3.rds")
 res_block <- readRDS("simul_results/static_dgp/block_stat_3.rds")
 
+res_stand$log_model_evidence
+res_block$log_model_evidence
 
 
-plot_theta_paths(res_block$theta_mean, res_block$theta_se, k=1, theta_true = 0.6)
+
+
+res_stand <- readRDS("simul_results/static_dgp/standard_8_pi07_M2000_G3_nmh5")
+res_block <- readRDS("simul_results/static_dgp/block_8_pi07_M2000_G3_nmh5")
+
+res_stand$
+
+
+plot_theta_paths(res_block$theta_mean, res_block$theta_se, k=1, theta_true = 0.5)
+
+
 
 
 k=1
 df_mean <- data.frame(
-  block = res_block$gamma_mean[, k],
-  stand = res_stand$gamma_mean[, k]
+  block = res_block$theta_mean[, k],
+  stand = res_stand$theta_mean[, k]
 )
 
 df_se <- data.frame(
-  block = res_block$gamma_se[, k],
-  stand = res_stand$gamma_se[, k]
+  block = res_block$theta_se[, k],
+  stand = res_stand$theta_se[, k]
 )
+true_values_list = list(0.66,  0.70,  0.53,  0.38,  0.70,  0.70,  0.58)
+p <- plot_theta_paths_df(df_mean, df_se, theta_true=0.7, legend_labels = c("Tempering", "Standard"), 
+                         plot_title = expression("Posterior path of " * theta[12]))
 
-p <- plot_theta_paths_df(df_mean, df_se, theta_true=1, legend_labels = c("Tempering", "Standard"), plot_title = expression("Posterior path of " * gamma[12]))
-ggsave(filename = "figures/static_3d_gamma12.png", plot = p, width = 8, height = 6)
+library(patchwork)
+
+true_values_list <- rev(list(0.03,  0.15,  0.20,  0.00,  0.00))
+true_values_list <- rev(list(0.2,  0.15,  0.03,  0.00,  0.00))
+true_values_list <- rev(list(0,0,0,0))
+
+
+p <- 18
+plots <- lapply(seq_along(true_values_list), function(k) {
+  df_mean <- data.frame(
+    block = res_block$gamma_mean[, k+p],
+    stand = res_stand$gamma_mean[, k+p]
+  )
+  
+  df_se <- data.frame(
+    block = res_block$gamma_se[, k+p],
+    stand = res_stand$gamma_se[, k+p]
+  )
+  
+  plot_theta_paths_df(
+    theta_mean = df_mean,
+    theta_sd   = df_se,
+    theta_true = true_values_list[[k]],
+    legend_labels = c("Tempering", "Standard"),
+    plot_title = bquote("Posterior path of " * gamma[.(k)])
+  )
+})
+
+# Add empty placeholders to fill 3x3 layout
+while (length(plots) < 4) {
+  plots[[length(plots) + 1]] <- patchwork::plot_spacer()
+}
+
+# Combine and display in a 3x3 grid
+combined_plot <- wrap_plots(plots, ncol = 2)
+combined_plot
+
+
+ggsave(filename = "figures/static_8d_tr4_gamma.png", plot = combined_plot, width = 12, height = 12) 
 
 
 df_ESS <- data.frame(
   block = res_block$diag_log$ESS,
-  stand = res_stand$diag_log$ESS,
-  threshold = rep(500, length( res_stand$diag_log$ESS))
+  #stand = res_stand$diag_log$ESS,
+  threshold = rep(1000, length( res_stand$diag_log$ESS))
 )
 
-p <- plot_theta_paths_df(df_ESS,  plot_title = "Effective Sample Size (Hits: 17 for standard and 22 for blocks)")
-ggsave(filename = "figures/static_3d_ESS.png", plot = p, width = 8, height = 6)
+df_ESS <- df_ESS[1:3000,]
 
+sum(df_ESS$block<1000)
+
+p <- plot_theta_paths_df(df_ESS,  plot_title = "Effective Sample Size - 169 hits")
+ggsave(filename = "figures/static_8d_ESS_block.png", plot = p, width = 8, height = 6)
 
 
 df_unique <- data.frame(
   block = res_block$diag_log$unique,
-  stand = res_stand$diag_log$unique,
-  initial_value = rep(1000, length( res_stand$diag_log$unique))
+  #stand = res_stand$diag_log$unique,
+  initial_value = rep(2000, length( res_stand$diag_log$unique))
 )
+
+df_unique <- df_unique[1:3000,]
+
 p <- plot_theta_paths_df(df_unique,  plot_title = "Unique number of particles")
-ggsave(filename = "figures/static_3d_unique.png", plot = p, width = 8, height = 6)
-
-
-res_stand$log_model_evidence
-
-
+ggsave(filename = "figures/static_3d_unique_block.png", plot = p, width = 8, height = 6)
 
 
 
